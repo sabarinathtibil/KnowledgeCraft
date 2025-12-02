b42c70c805a74f77ef44f386fbd8fb9379fc6cbe
sabarinathtibil <sabarinath.m@tibilsolutions.com>
2025-12-02 11:35:37 +0530

    login page

--- src/app/Components/login/login.ts
+++ src/app/Components/login/login.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
