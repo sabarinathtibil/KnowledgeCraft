622612c0288ab3a0521e624effd8930c3deac015
sabarinathtibil <sabarinath.m@tibilsolutions.com>
2025-12-02 21:38:12 +0530

    lessonTemplate_Completed

---
+++ src/app/Services/quest-service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class QuestService {
+
+  constructor(private http:HttpClient){}
+
+  questURL = 'http://localhost:3000/quests';
+
+  getQuest(){
+    this.http.get(this.questURL)
+  }
+
+  postQuest(data:any){
+    return this.http.post(this.questURL,data)
+  }
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { Login } from './Components/login/login';
 import { Register } from './Components/register/register';
 import { Home } from './Components/home/home';
+import { Quests } from './Components/quests/quests';
+import { Badge } from './Components/badge/badge';
+import { Lessons } from './Components/lessons/lessons';
 
 export const routes: Routes = [
   {path:'login',component:Login},
   {path:'register',component:Register},
   {path:'home',component:Home},
+  {path:"badge" , component:Badge},
+  {path:"quest" , component:Quests},
+  {path:"lessons" , component:Lessons},
   { path: '**', redirectTo: 'login' },
 ];
