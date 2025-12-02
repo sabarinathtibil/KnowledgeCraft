7b3ff7ee8840549efe1f55558041151fdb0365ab
shashankr-tibil <shashank.r@tibilsolutions.com>
2025-12-02 11:11:53 +0530

    deleted old files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,5 @@
 import { Routes } from '@angular/router';
-import { LoginPage } from './Components/login-page/login-page';
-import { Course } from './Components/course/course';
-import { LandingPage } from './Components/landing-page/landing-page';
-import { AuthGuard } from './Services/auth-guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginPage },
-
-  {
-    path: 'home',
-    component: LandingPage,
-    canActivate: [AuthGuard],
-    children: [
-      { path: 'course', component: Course }
-    ],
-  },
-
-  // Default redirect to login
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-  // Must be last → handles invalid paths
   { path: '**', redirectTo: 'login' },
 ];
